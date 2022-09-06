024ed906a6d7b6d8f26c42c8e45ffa6ba48e9a49
Albin Södervall <91470009+albinsoder@users.noreply.github.com>
2022-09-06 15:02:10 +0200

    compdir change

--- compdir.sh
+++ compdir.sh
@@ -1,8 +1,33 @@
 #! /bin/bash
 
-input="$1"
-pwdHolder=$(pwd)
-echo $pwdHolder
+input="$1" # Dir name
+pwdHolder=$(pwd) # Current pwd
+let data="512000" # 512MB
+# echo $pwdHolder
+
+# Create archive file of input directory
+#c - Create
+#x - Extract
+#t - List contents
+#v - Output dir contents
+#f - Use archive file
+#z - Filter the archive through gzip
+archivator() {
+    tar -cf "$input.tgz" "$input" #c=create, f=Using archive file, gzip used for compressing
+    echo "Directory $input archived as $input.tgz"
+}
+
+exitAll() {
+    grep -o . <<< "$input" | while read letter;  
+    do
+        if [ "$letter" == "/" ]
+        then 
+            echo 1
+            exit 0
+        fi
+    done
+    echo 2
+}
 
 # Basic checks for the input arg provided by user
 if [ "$#" -gt 1 ];
@@ -17,28 +42,42 @@ then
 fi
 concat="${pwdHolder}/${input}"
 echo "$concat"
-size="$(du $input | awk '{print $1}')"
-echo "$size"
+dirCheck=0
+grep -o . <<< "$input" | while read letter;  
+do
+    if [ "$letter" == "/" ]
+    then 
+        dirCheck=1
+    fi
+done <<<$(find tmp -type f)
+
+echo "$dirCheck"
+if [ "$dirCheck" == "1" ]
+then    
+    size="$(du $input | awk '{print $1}')" # split input, choose to print size of input dir
+    echo "$size"
+fi
+
 if [ -d "$concat" ]
 then
     if [ -w "$pwdHolder" ]
     then
-        if [ "$size" -gt 512 ]
+        if [ "$size" -gt "$data" ]
         then 
-            echo "BIG BOY, wanna proceed? y/n"
+            echo "Warning: the directory is 512 MB. Proceed? [y/n]"
             read userInput
-            if [ "$userInput" == "y" ]
+            if [ "$userInput" == "y" | "$userInput" == "Y" ]
             then
-                echo "Grattis kalle"
+                archivator # archivate the dir
             else
-                echo "We done here"
+                echo "You have selected to not create an archive file"
             fi
         else    
-            echo "COMMON L"
+            archivator # archivate the dir
         fi
     else 
-        echo "Always L"
+        echo "No writing permission in parent dir"
     fi   
-else 
-    echo "common L(also known as depression)"
+else
+    echo "Cannot find directory $input"
 fi
